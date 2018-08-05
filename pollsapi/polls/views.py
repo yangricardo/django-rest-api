8a6f9e97dc8261e7b3210b846151a13a15d9815f
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2018-08-05 18:18:44 -0300

    apiview

--- pollsapi/polls/views.py
+++ pollsapi/polls/views.py
@@ -1,23 +1,19 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import JsonResponse
-from polls.models import Poll
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from .models import Poll, Choice
+from .serializers import PollSerializer
 
 
-def polls_list(request):
-    MAX_OBJECTS = 20
+class PollList(APIView):
+    def get(self, request):
     polls = Poll.objects.all()[:20]
-    data = {"results": list(polls.values(
-        "question", "created_by__username", "pub_date"))}
-    return JsonResponse(data)
+    data = PollSerializer(polls, many=True).data
+    return Response(data)
 
 
-def polls_detail(request, pk):
-    poll = get_object_or_404(Poll, pk=pk)
-    data = {
-        "results": {
-            "question": poll.question,
-            "created_by": poll.created_by.username,
-            "pub_date": poll.pub_date
-        }
-    }
-    return JsonResponse(data)
+class PollDetail(APIView):
+    def get(self, request, pk):
+        poll = get_object_or_404(Poll, pk=pk)
+        data = PollSerializer(poll).data
+        return Response(data)
