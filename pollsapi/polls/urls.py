90559d6cbe6c1fe81bf653012a0aa9532cb3b7a4
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2018-08-05 18:19:59 -0300

    apiview

--- pollsapi/polls/urls.py
+++ pollsapi/polls/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from . import views
+from .views import PollList, PollDetail
 
 urlpatterns = [
-    path("polls/", views.polls_list, name="polls_list"),
-    path("polls/<int:pk>/", views.polls_detail, name="polls_detail")
+    path("polls/", PollList.as_view(), name="polls_list"),
+    path("polls/<int:pk>/", PollDetail.as_view(), name="polls_detail")
 ]
--- pollsapi/polls/views.py
+++ pollsapi/polls/views.py
@@ -7,9 +7,9 @@ from .serializers import PollSerializer
 
 class PollList(APIView):
     def get(self, request):
-    polls = Poll.objects.all()[:20]
-    data = PollSerializer(polls, many=True).data
-    return Response(data)
+        polls = Poll.objects.all()[:20]
+        data = PollSerializer(polls, many=True).data
+        return Response(data)
 
 
 class PollDetail(APIView):
